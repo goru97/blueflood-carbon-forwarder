507a60c20d12a7887cd63a646a8a2fa39f4febc2
Gaurav Bajaj <goru97@gmail.com>
2015-07-28 17:03:09 -0700

    Final fix

--- carbonforwarderlogging/forwarder_log_observer.py
+++ carbonforwarderlogging/forwarder_log_observer.py
@@ -3,7 +3,7 @@ import os
 from twisted.logger import FilteringLogObserver, LogLevelFilterPredicate, LogLevel, jsonFileLogObserver
 from twisted.python import logfile
 
-log_dir = os.environ.get("LOG_DIR", '/opt/twistedlogs/')
+log_dir = os.environ.get("LOG_DIR", '/var/log/')
 log_level = os.environ.get("TWISTED_LOG_LEVEL", 'INFO').lower()
 log_rotate_length = os.environ.get("LOG_ROTATE_LENGTH", 100000)
 max_rotated_log_files = os.environ.get("MAX_LOG_ROTATED_FILES", 100)
@@ -13,5 +13,5 @@ def get_log_observer():
     observer = jsonFileLogObserver(f)
     filterer = FilteringLogObserver(observer,
         [LogLevelFilterPredicate(
-            LogLevel.levelWithName())])
+            LogLevel.levelWithName(log_level))])
     return filterer
